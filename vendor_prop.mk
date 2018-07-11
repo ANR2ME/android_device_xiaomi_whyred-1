3720e4656b7a6cc506f0b0ef2bd345e400078e25
Subhaam <derksoul98@gmail.com>
2018-07-11 17:06:48 +0530

    whyred: Update security patch level from MIUI 9.5.17 Global Stable
    Change-Id: I4288106fe0b8fa2611fa15eaca2010c0c48b958a
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -223,7 +223,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Security Patch Level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2018-05-01
+    ro.vendor.build.security_patch=2018-06-01
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
